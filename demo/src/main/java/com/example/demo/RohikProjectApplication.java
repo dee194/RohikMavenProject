255c13b38c6c855afaa338e759abc30472f9cbbf
dee194 <rohik2593@gmail.com>
2022-09-12 12:37:14 +0200

    Some Changes

--- demo/src/main/java/com/example/demo/RohikProjectApplication.java
+++ demo/src/main/java/com/example/demo/RohikProjectApplication.java
@@ -9,7 +9,6 @@ public class RohikProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RohikProjectApplication.class, args);
 		System.out.println("Change");
-		System.out.println("Change2");
 	}
 
 }
